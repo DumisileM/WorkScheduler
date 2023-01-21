bed35fc8e63ba0f823f2dde6c339855e14193f2e
DumisileM <dumisilem@aol.com>
2023-01-21 00:29:57 +0000

    started color  coding function

--- starter/script.js
+++ starter/script.js
@@ -1,28 +1,54 @@
-//Display cuttrnt date at the top of page.
-var day = moment().format("MMM Mo, YYYY")
-$('#currentDay').text(day)
-
-function assignHour(element,hour){
-        element.text(hour)
+var currentHour = $('.hour');
+//Display current date at the top of page.
+function getCurrentDay(){
+    var day = moment().format("MMM Mo, YYYY");
+    $('#currentDay').text(day);
 }
 
-for(let i=9; i < 18;){
- 
-  $( ".hour" ).each(function( index ) {
-    var hour = moment().hours(i).format("k a");
-    i++
-    $(this).text(hour)
-    console.log( index + ": " + $( this ).text() );
-  });
 
+function setHours(){
+    /*Present timeblocks for standard business hours when the 
+    user scrolls down. This fucntions sets typical business hours(9-5)
+    and displays them on timeblocks*/
+    for(let i=20; i < 24;){
+ 
+        $( ".hour" ).each(function( index ) {
+          var hour = moment().hours(i).format("HH a");
+          i++
+          $(this).text(hour)
          
-    }
+        });
+      
+               
+          };
+
+}
+
+function setPastPresentFuture(){
+    currentHour.each(function(){
+       
+
+        if(Number($(this).text().slice(0,2)) == moment().hour()){
+            
+            $(this).next().addClass("present");
+        }else if(Number($(this).text().slice(0,2)) > moment().hour()){
+            
+            $(this).next().addClass("future");
+        }else if(Number($(this).text().slice(0,2)) < moment().hour()){
+          
+            $(this).next().addClass("past");
+        }
+    })
+    };
+
+    
     
 
 
   
-
-
+getCurrentDay()
+setHours()
+setPastPresentFuture()
 
 
 
