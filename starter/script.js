535828ffb7c7f8c250bf231f54b3f984c8f72c39
DumisileM <dumisilem@aol.com>
2023-01-22 01:48:31 +0000

    finished color-coding function and started function to save input to localstorage

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,8 @@
-var currentHour = $('.hour');
+var currentHour = $('.hour')
+var currentTextArea = ($('.time-block'))
+
+
+
 //Display current date at the top of page.
 function getCurrentDay(){
     var day = moment().format("MMM Mo, YYYY");
@@ -10,10 +14,11 @@ function setHours(){
     /*Present timeblocks for standard business hours when the 
     user scrolls down. This fucntions sets typical business hours(9-5)
     and displays them on timeblocks*/
-    for(let i=20; i < 24;){
+    for(let i=9; i < 18;){
  
         $( ".hour" ).each(function( index ) {
           var hour = moment().hours(i).format("HH a");
+          
           i++
           $(this).text(hour)
          
@@ -26,15 +31,20 @@ function setHours(){
 
 function setPastPresentFuture(){
     currentHour.each(function(){
-       
-
-        if(Number($(this).text().slice(0,2)) == moment().hour()){
+        var formattedHour = moment().format("YYYY-MM-DD " + `${$(this).text().slice(0,3)}:mm:ss`.replace(/\s+/g, ''))
+        console.log(moment(formattedHour).isValid())
+        var today = moment(moment().format("YYYY-MM-DD HH:mm:ss"))
+        
+        // var test = moment("2023-01-22 09:00:00","YYYY-MM-DD HH:mm:ss")
+        
+        if(today.isSame(formattedHour,"hour")){
             
             $(this).next().addClass("present");
-        }else if(Number($(this).text().slice(0,2)) > moment().hour()){
+        }else if(today.isBefore(formattedHour,"hour")){
             
             $(this).next().addClass("future");
-        }else if(Number($(this).text().slice(0,2)) < moment().hour()){
+        }else if(today.isAfter(formattedHour,"hour")){
+            console.log("past")
           
             $(this).next().addClass("past");
         }
@@ -43,12 +53,33 @@ function setPastPresentFuture(){
 
     
     
+function makeEntryAndSave(){
+    currentTextArea.each(function(){
+        $(this).on("keydown",function(){
+            console.log($(this).val())
+            localStorage.setItem("activity",$(this).val());
+            var activity = localStorage.getItem("activity")
+            $(this).text(activity)
+            return activity
+
+        })
+        
+        
+       
 
 
+    })
+
+}
+function getEntry(){
+    
+}
   
 getCurrentDay()
 setHours()
 setPastPresentFuture()
+makeEntryAndSave()
+
 
 
 
